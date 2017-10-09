ca0e9a5876bcdda26ff8af9068b7eb812b26b372
Melkiah <desIvanovPetrov@gmail.com>
2017-10-09 22:31:44 +0300

    Completed FoldAndSum task

--- ArraysExercises/FoldAndSum/FoldAndSum.cs
+++ ArraysExercises/FoldAndSum/FoldAndSum.cs
@@ -10,6 +10,37 @@ namespace FoldAndSum
     {
         static void Main(string[] args)
         {
+            string[] inputArray = Console.ReadLine().Split(' ');
+            int main = inputArray.Length / 2;
+            int k = main / 2;
+            int count = 0;
+
+            int[] middleArr = new int[main];
+            int[] sidesArr = new int[main];
+
+            for (int i = k; i < k+main; i++)
+            {
+                middleArr[count] += int.Parse(inputArray[i]);
+                count++;
+            }
+            count = 0;
+
+            for(int i = k-1; i >= 0; i--)
+            {
+                sidesArr[count] += int.Parse(inputArray[i]);
+                count++;
+            }
+            
+            for (int i = inputArray.Length-1; i >= k+main; i--)
+            {
+                sidesArr[count] += int.Parse(inputArray[i]);
+                count++;
+            }
+
+            for (int i = 0; i < main; i++)
+            {
+                Console.Write("{0} ", middleArr[i]+sidesArr[i]);
+            }
         }
     }
 }
