95d8dc5f2b5bee8aebc9766dc54391a629a916bc
Melkiah <desIvanovPetrov@gmail.com>
2017-10-09 21:58:56 +0300

    Completed Rotate and Sum

---
+++ ArraysExercises/RotateAndSum/RotateAndSum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateAndSum
+{
+    class RotateAndSum
+    {
+        static void Main(string[] args)
+        {
+            string[] toRotate = Console.ReadLine().Split(' ');
+            int rotations = int.Parse(Console.ReadLine());
+            int arrayLength = toRotate.Length;
+
+            string[] rotated = new string[arrayLength];
+            int[] sumArray = new int[arrayLength];
+
+            for (int j = 1; j <= rotations; j++)
+            {
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    rotated[(i + j) % arrayLength] = toRotate[i];
+                }
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    sumArray[i] += int.Parse(rotated[i]);
+                }
+            }
+
+            foreach (int number in sumArray)
+            {
+                Console.Write(number +" ");
+            }
+        }
+    }
+}
