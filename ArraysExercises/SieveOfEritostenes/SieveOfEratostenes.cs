99b5b0fdab74d7d8cc8c3bf07400a7e0d21efa32
Melkiah <desIvanovPetrov@gmail.com>
2017-10-10 22:32:23 +0300

    Sieve of Eritostenes completed

--- ArraysExercises/SieveOfEritostenes/SieveOfEratostenes.cs
+++ ArraysExercises/SieveOfEritostenes/SieveOfEratostenes.cs
@@ -10,6 +10,36 @@ namespace SieveOfEritostenes
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            bool[] primes = new bool[n+1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                if(i == 0 || i == 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    primes[i] = true;
+                }
+            }
+            
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (primes[i])
+                {
+                    Console.Write(i + " ");
+
+                    for (int j = i*i; j <= n; j = j + i)
+                    {
+                        primes[j] = false;
+                    }
+                }
+            }
+            
+
         }
     }
 }
