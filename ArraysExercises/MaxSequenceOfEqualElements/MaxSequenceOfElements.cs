b33673321fcb5d563e1ee3fbd170b5c5437183b9
Melkiah <desIvanovPetrov@gmail.com>
2017-10-12 20:57:30 +0300

    Completed Max Sequence Of Equal Elements

--- ArraysExercises/MaxSequenceOfEqualElements/MaxSequenceOfElements.cs
+++ ArraysExercises/MaxSequenceOfEqualElements/MaxSequenceOfElements.cs
@@ -10,6 +10,34 @@ namespace MaxSequenceOfEqualElements
     {
         static void Main(string[] args)
         {
+            string[] inputArray = Console.ReadLine().Split(' ');
+            int largest = 0;
+            int last = 0;
+            int len = 1;
+
+            for (int i = 0; i < inputArray.Length-1; i++)
+            {
+                if(inputArray[i].Equals(inputArray[i + 1]))
+                {
+                    len++;
+                    if (len > largest)
+                    {
+                        largest = len;                        
+                        last = i+1;
+                    }
+                }
+                else
+                {
+                    len = 1;
+                }
+                
+            }
+
+            for (int i = last; i >= last-largest+1; i--)
+            {
+                Console.Write(inputArray[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
