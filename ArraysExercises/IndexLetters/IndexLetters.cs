5a06957e0cc901c98e866b4fdf731c4db5b7cbca
Melkiah <desIvanovPetrov@gmail.com>
2017-10-14 20:46:57 +0300

    Completed IndexLetter

---
+++ ArraysExercises/IndexLetters/IndexLetters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexLetters
+{
+    class IndexLetters
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            char[] alphabet = new char[26];
+            char letter = 'a';
+
+            for (int i = 0; i < 26; i++)
+            {
+                alphabet[i] = letter;
+                letter++;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (input[i]==alphabet[j])
+                    {
+                        Console.WriteLine(input[i] + " -> " + j);
+                        break;
+                    }
+                }
+            }
+            
+        }
+    }
+}
