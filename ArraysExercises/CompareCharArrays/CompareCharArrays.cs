f577707d6b8193481c7da3b527b714ceba37bf10
Melkiah <desIvanovPetrov@gmail.com>
2017-10-14 21:49:49 +0300

    Completed Compare Char Array

--- ArraysExercises/CompareCharArrays/CompareCharArrays.cs
+++ ArraysExercises/CompareCharArrays/CompareCharArrays.cs
@@ -10,6 +10,94 @@ namespace CompareCharArrays
     {
         static void Main(string[] args)
         {
+            char[] firstArray = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            char[] secondArray = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+
+            bool isFirst = false;
+
+            if (firstArray.Length < secondArray.Length)
+            {
+                for (int i = 0; i < firstArray.Length; i++)
+                {
+                    if (firstArray[i] <= secondArray[i])
+                    {
+                        isFirst = true;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (isFirst)
+                {
+                    foreach (var item in firstArray)
+                    {
+                        Console.Write(item);
+                    }
+                    Console.WriteLine();
+                    foreach (var item in secondArray)
+                    {
+                        Console.Write(item);
+                    }
+                }
+                else
+                {
+                    foreach (var item in secondArray)
+                    {
+                        Console.Write(item);
+                    }
+                    Console.WriteLine();
+                    foreach (var item in firstArray)
+                    {
+                        Console.Write(item);
+                    }
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i < secondArray.Length; i++)
+                {
+                    if (secondArray[i] <= firstArray[i])
+                    {
+                        isFirst = true;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (isFirst)
+                {
+                    foreach (var item in secondArray)
+                    {
+                        Console.Write(item);
+                    }
+                    Console.WriteLine();
+                    foreach (var item in firstArray)
+                    {
+                        Console.Write(item);
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    foreach (var item in firstArray)
+                    {
+                        Console.Write(item);
+                    }
+                    Console.WriteLine();
+                    foreach (var item in secondArray)
+                    {
+                        Console.Write(item);
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
