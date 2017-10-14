275ecb26d4c3c9fd0d1351287d34c92ed5a932c8
Melkiah <desIvanovPetrov@gmail.com>
2017-10-14 21:07:33 +0300

    Completed PairsByDifference :)

---
+++ ArraysExercises/PairsByDifference/PairsByDifference.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairsByDifference
+{
+    class PairsByDifference
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int difference = int.Parse(Console.ReadLine());
+
+            int pairsCount = 0;
+
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    if (inputArray[i] - inputArray[j] == difference || inputArray[j] - inputArray[i] == difference)  //1 5 3 4 2
+                    {
+                        pairsCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(pairsCount);
+        }
+    }
+}
