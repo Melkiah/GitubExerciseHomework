cff3bebccea35bcd3884ed67f77a87cbaa7de46e
Melkiah <desIvanovPetrov@gmail.com>
2017-10-14 21:25:00 +0300

    Completed EqualSum

--- ArraysExercises/EqualSum/EqualSum.cs
+++ ArraysExercises/EqualSum/EqualSum.cs
@@ -10,6 +10,45 @@ namespace EqualSum
     {
         static void Main(string[] args)
         {
+            int[] inputArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int leftSum = 0;
+            int rightSum = 0;
+            //int index = 0;
+            bool isFound = false;
+
+            for (int i = 0; i < inputArray.Length; i++) //10 5 5 99 3 4 2 5 1 1 4
+            {
+                for (int j = i - 1; j >= 0; j--)// sum to the left
+                {
+                    leftSum += inputArray[j];
+                }
+
+                for (int j = i + 1; j < inputArray.Length; j++) // sum to the right
+                {
+                    rightSum += inputArray[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    isFound = true;
+                    break;
+                }
+
+                leftSum = 0;
+                rightSum = 0;
+            }
+
+            //leftSum = -1;
+
+            if (!isFound)
+            {
+                Console.WriteLine("no");
+            }
         }
     }
 }
