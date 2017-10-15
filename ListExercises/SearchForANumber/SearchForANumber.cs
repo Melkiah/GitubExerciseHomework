fe0622700e9440c66c96934a89254354c9900b61
Melkiah <desIvanovPetrov@gmail.com>
2017-10-15 12:24:11 +0300

    Completed BombNumbers, SumOfReversedNumbers, SearchForAnumber

---
+++ ListExercises/SearchForANumber/SearchForANumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchForANumber
+{
+    class SearchForANumber
+    {
+        static void Main(string[] args)
+        {
+
+            string yes = "YES!";
+            string no = "NO!";
+
+            bool isFound = false;
+            int[] commands = new int[3];
+
+            List<int> numbersArray = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            commands = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = commands[1]; i < commands[0]; i++)
+            {
+                if (numbersArray[i] == commands[2])
+                {
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (isFound)
+            {
+                Console.WriteLine(yes);
+            }
+            else
+            {
+                Console.WriteLine(no);
+            }
+        }
+    }
+}
---
+++ ListExercises/SumOfReversedNumbers/SumOfReversedNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfReversedNumbers
+{
+    class SumOfReversedNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersArray = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int sum = 0; int reversed = 0; int number;
+
+            for (int i = 0; i < numbersArray.Count; i++)
+            {
+                int lastdigit = 0;
+                number = numbersArray[i];
+                while (number > 0)
+                {
+                    lastdigit = number % 10;
+                    reversed = reversed * 10 + lastdigit;
+                    number /= 10;
+                }
+
+                sum += reversed;
+                reversed = 0;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
