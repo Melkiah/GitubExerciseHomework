fda306d3c3766027cd14693335ccafc569e621f0
Melkiah <desIvanovPetrov@gmail.com>
2017-10-15 11:02:55 +0300

    Completed MaxSequenceWithLists

--- ListExercises/MaxSeuquenceWithLists/MaxSequenceWithLists.cs
+++ ListExercises/MaxSeuquenceWithLists/MaxSequenceWithLists.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace MaxSeuquenceWithLists
 {
-    class Program
+    class MaxSequenceWithLists
     {
         static void Main(string[] args)
         {
+            List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int start = 0, len = 1, bestStart = 0, bestLen = 1;
+
+            for (int i = 1; i < input.Count; i++)
+            {
+                if (input[i] == input[i - 1])
+                {
+                    len++;
+                }
+                else
+                {
+                    len = 1;
+                    start = i;
+                }
+
+                if (len > bestLen)
+                {
+                    bestLen = len;
+                    bestStart = start;
+                }
+            }
+
+            for (int i = bestStart; i <= bestLen + bestStart - 1; i++)
+            {
+                Console.Write(input[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
