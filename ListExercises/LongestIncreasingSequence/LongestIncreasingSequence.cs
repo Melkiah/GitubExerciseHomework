dcd6dcaecce2d8c0a013bc195ebac72d0c92d956
Melkiah <desIvanovPetrov@gmail.com>
2017-10-15 16:34:31 +0300

    Still have no clue What to do

--- ListExercises/LongestIncreasingSequence/LongestIncreasingSequence.cs
+++ ListExercises/LongestIncreasingSequence/LongestIncreasingSequence.cs
@@ -10,6 +10,12 @@ namespace LongestIncreasingSequence
     {
         static void Main(string[] args)
         {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int n = input.Count;
+            int[] nums = new int[n];
+            List<int> len = new List<int>();
+
+            
         }
     }
 }
