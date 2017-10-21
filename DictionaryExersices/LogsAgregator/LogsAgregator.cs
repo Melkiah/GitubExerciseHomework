0761b653f3b62b811e46019a5572d5734a07aa7e
Melkiah <desIvanovPetrov@gmail.com>
2017-10-21 22:21:31 +0300

    Logs Agreggator Initial commit

---
+++ DictionaryExersices/LogsAgregator/LogsAgregator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogsAgregator
+{
+    class LogsAgregator
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
