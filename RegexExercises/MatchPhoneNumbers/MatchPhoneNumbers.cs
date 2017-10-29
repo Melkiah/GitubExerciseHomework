5a706ecefdcbf071e593cae1b1859bf260d7f730
Melkiah <desIvanovPetrov@gmail.com>
2017-10-29 15:23:30 +0200

    Complete RegexPhoneNumbers

--- RegexExercises/MatchPhoneNumbers/MatchPhoneNumbers.cs
+++ RegexExercises/MatchPhoneNumbers/MatchPhoneNumbers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MatchPhoneNumbers
@@ -10,6 +11,14 @@ namespace MatchPhoneNumbers
     {
         static void Main(string[] args)
         {
+            string regex = @"\+359( |-)2(\1)\d{3}(\1)\d{4}\b";
+            string input = "+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222";
+
+            MatchCollection matchedPhonenumbers = Regex.Matches(input, regex);
+
+            string[] collection = matchedPhonenumbers.Cast<Match>().Select(x => x.Value.ToString()).ToArray();
+
+            Console.WriteLine(String.Join(", ", collection));            
         }
     }
 }
