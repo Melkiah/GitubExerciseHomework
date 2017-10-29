0365c430b01137684afeb13f249caf23e8498d10
Melkiah <desIvanovPetrov@gmail.com>
2017-10-29 14:59:58 +0200

    MatchNames completed

--- RegexExercises/RegexMatchNames/MatchNames.cs
+++ RegexExercises/RegexMatchNames/MatchNames.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RegexMatchNames
 {
-    class Program
+    class MatchNames
     {
         static void Main(string[] args)
         {
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string input = "Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov";
+
+            MatchCollection matchedNames = Regex.Matches(input, regex);
+
+            foreach (Match item in matchedNames)
+            {
+                Console.WriteLine(item.Value + " ");
+            }
         }
     }
 }
